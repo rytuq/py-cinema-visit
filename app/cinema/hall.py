4feb429f19fcd84e24e88ea479489aa6efdacf58
inc <inc@email.com>
2026-09-04 21:02:04 +0300

    /

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,7 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
@@ -5,8 +9,8 @@ class CinemaHall:
     def movie_session(
         self,
         movie_name: str,
-        customers: list,
-        cleaning_staff: list
+        customers: list[Customer],
+        cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
